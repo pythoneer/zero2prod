6360e909484a75e4def77847243a4bf2d0818093
Dustin Bensing <dustin.bensing@googlemail.com>
2020-11-18 23:28:12 +0100

    tracing

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,14 @@ use sqlx::PgPool;
 
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+    // tracing
+    let subscriber = telemetry::get_subscribers("zero2prod".into(), "info".into());
+    telemetry::init_subscribers(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,6 +5,15 @@ use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscribers, init_subscribers};
+
+lazy_static::lazy_static! {
+    static ref TRACING: () = {
+        let filter = if std::env::var("TEST_LOG").is_ok() { "debug" } else { "" };
+        let subscriber = get_subscribers("test".into(), filter.into());
+        init_subscribers(subscriber);
+    };
+}
 
 pub struct TestApp {
     pub address: String,
@@ -12,6 +21,8 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    lazy_static::initialize(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
     // We retrieve the port assigned to us by the OS
